b4353886b793e8ef6be89c3255800b03c551c04a
Bradley J Chambers <brad.chambers@gmail.com>
2014-07-25 09:56:54 -0400

    first port of code

--- src/Saga.jl
+++ src/Saga.jl
@@ -1,5 +1,14 @@
 module Saga
 
-# package code goes here
+include("io_gdal.jl")
+include("ta_hydrology.jl")
+include("ta_lighting.jl")
+include("ta_morphometry.jl")
+include("ta_slope_stability.jl")
+include("utilities.jl")
+
+function Saga()
+    println("help")
+end
 
 end # module
